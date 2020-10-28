936066089c2ae1654eaf5038b8671819bef53e6d
flplucio <45046100+flplucio@users.noreply.github.com>
2020-10-28 16:03:52 +0000

    Update PdfBoxPlugin.java

--- src/android/PdfBoxPlugin.java
+++ src/android/PdfBoxPlugin.java
@@ -11,3 +11,47 @@ import java.io.ByteArrayOutputStream;
 import java.util.Set;
 import java.util.List;
 
+public class PdfBoxPlugin extends CordovaPlugin {
+
+    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+
+        // add_image
+        if (action.equals("add_image")) {
+
+            try {
+                
+                //Here goes the code that will merge the client sign image with the VV PDF
+
+                callbackContext.success(pdfout_base64);
+
+            } catch (Exception e) {
+                
+
+                callbackContext.error(e.toString());
+            }
+
+            return true;
+        }
+        
+        // get_position
+        else if (action.equals("get_position")) {
+
+            try {
+
+                //Here goes the code that will get the image position
+
+                callbackContext.success("{\"posX\" : \"" + posX + "\", \"posY\" : \"" + posY + "\"}");
+
+            } catch (Exception e) {
+                callbackContext.error(e.toString());
+            }
+
+            return true;
+        } else {
+
+            return false;
+
+        }
+    }
+
+}
