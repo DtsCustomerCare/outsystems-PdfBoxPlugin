e29bc318505782ba99127ec2db97979e2d7a0438
flplucio <45046100+flplucio@users.noreply.github.com>
2021-02-19 15:03:33 +0000

    base64 conversions

--- src/android/PdfBoxPlugin.java
+++ src/android/PdfBoxPlugin.java
@@ -41,10 +41,10 @@ public class PdfBoxPlugin extends CordovaPlugin {
                 PDRectangle rectangle = getPosition();
 
 
-                    PDDocument doc = PDDocument.load(pdf_base64);
+                    PDDocument doc = PDDocument.load((Base64.getDecoder().decode(pdf_base64));
                     PDPage page = doc.getPage(0);
                     //PDImageXObject pdImage = PDImageXObject.createFromFile(imagePath, doc);
-                    PDImageXObject pdImage = PDImageXObject.createFromByteArray(doc, png_base64, "");
+                    PDImageXObject pdImage = PDImageXObject.createFromByteArray(doc, (Base64.getDecoder().decode(png_base64), "");
 
                     try (PDPageContentStream contentStream = new PDPageContentStream(doc, page, AppendMode.APPEND, true, true))
                     {
@@ -98,7 +98,7 @@ public class PdfBoxPlugin extends CordovaPlugin {
                 PDDocumentCatalog docCatalog = pdfDocument.getDocumentCatalog();
                 PDAcroForm acroForm = docCatalog.getAcroForm();
                 
-                PDField f = acroForm.getFields().get(0);
+                PDField f = (PDField)   acroForm.getFields().get(0);
             
                 PDRectangle r = f.getWidgets().get(0).getRectangle();
 
